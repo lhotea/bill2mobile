66c5d70f0bcaceb3c5c1bf4f2c16091ac7f40ede
lhotea <arnaud@lhote.ch>
2014-07-20 15:21:08 -0400

    changed from modal to view for invoiceDetail

--- js/bill2mobile.js
+++ js/bill2mobile.js
@@ -1,72 +1,90 @@
-		
-var app = angular.module('myApp', ['ionic']);
-app.config(function($stateProvider, $urlRouterProvider) {
-  $stateProvider
-  .state('login', {
-    url: '/',
-    templateUrl: 'partials/login.html',
-	controller: 'loginController',
-	resolve: {
-	 customer: function(customerService) {
-	  return customerService.getCustomer()
-	 },
-	 debug: function(debugService) {
-	  return debugService.getDebug()
-	 } 
-	}
-  })
-    .state('account', {
-    url: '/account',
-    templateUrl: 'partials/account.html',
-	controller: 'accountController',
-	resolve: {
-	 customer: function(customerService) {
-	  return customerService.getCustomer()
-	 },
-	 debug: function(debugService) {
-	  return debugService.getDebug()
-	 } 
-	}
+		var app = angular.module('myApp', ['ionic']);
+        $ionicPlatform.registerBackButtonAction(function () { /* do nothing */}, 100);
+		app.config(function($stateProvider, $urlRouterProvider) {
+			$stateProvider
+				.state('login', {
+					url: '',
+					templateUrl: 'partials/login.html',
+					controller: 'loginController',
+					resolve: {
+						customer: function(customerService) {
+							return customerService.getCustomer();
+						},
+						debug: function(debugService) {
+							return debugService.getDebug();
+						}
+					}
+				})
+				.state('account', {
+					url: '/account',
+					templateUrl: 'partials/account.html',
+					controller: 'accountController',
+					resolve: {
+						customer: function(customerService) {
+							return customerService.getCustomer();
+						},
+						debug: function(debugService) {
+							return debugService.getDebug();
+						}
+					}
 
-});
-  $urlRouterProvider.otherwise('/');
-  
-});
+				})
+				.state('invoice', {
+					url: '/invoice/:invoice',
+					templateUrl: 'partials/invoiceDetail.html',
+					controller: 'invoiceController',
+					resolve: {
+						invoice: function(customerService,$stateParams) {
+							return customerService.getInvoice($stateParams.invoice);
+						},
+						customer: function(customerService) {
+							return customerService.getCustomer();
+						}
+					}
 
-/* services to share data between views */
-
-	app.service ('customerService', function($q) {
-		return {
-		 customer: {
-		  currency: 'CHF'
-		 },
-		 getCustomer: function() {
-		  return this.customer
-		 }
-		}
-	});
-
-	app.service ('debugService', function($q) {
-		return {
-		 debug: {
-		  request: {},
-		  response: {},
-		  debug: 0
-		 },
-		 getDebug: function() {
-		  return this.debug
-		 },
-		}
-	});
-
-/* controllers */
-
-		app.controller('loginController', function($scope, customer, debug, $http) {
-            $scope.cred = {
-			 username: "arnaud.lhote@hispeed.ch",
-			 password: "minisoph70"
-			};
+  });
 			
+		//	$urlRouterProvider.otherwise('');
+
+		});
+
+		/* services to share data between views */
+
+		app.service('customerService', function($q) {
+			return {
+				customer: {
+					currency: 'CHF'
+				},
+				getCustomer: function() {
+					return this.customer;
+				},
+				getInvoice: function(index) {
+					return this.customer.account.invoices[index];
+				}
+			};
+		});
+
+		app.service('debugService', function($q) {
+			return {
+				debug: {
+					request: {},
+					response: {},
+					debug: 0
+				},
+				getDebug: function() {
+					return this.debug;
+				},
+			};
+		});
+
+		/* controllers */
+
+		app.controller('loginController', function($scope, customer, debug, $http, $state) {
+			$scope.cred = {
+				username: "arnaud.lhote@hispeed.ch",
+				password: "minisoph70"
+			};
+
 			$scope.login = function() {
 				var request = {
 					language: 'en',
@@ -76,10 +94,10 @@ app.config(function($stateProvider, $urlRouterProvider) {
 					},
 					version: '1.1.7'
 				};
-				
+
 				$scope.request = JSON.stringify(request);
-//DEBUG INFO
-                debug.request = $scope.request;
+				//DEBUG INFO
+				debug.request = $scope.request;
 				debug.debug = $scope.cred.debug;
 
 				$http({
@@ -92,55 +110,61 @@ app.config(function($stateProvider, $urlRouterProvider) {
 					// when the response is available
 
 					$scope.response = JSON.stringify(data);
-//DEBUG INFO
-                    debug.response = $scope.response;
-					
-					if ( angular.isDefined(data.customer) && data.loginInfo == "authenticated"  )
-					{
-					 customer.account = data.customer.accounts[0];
-                     window.location.href ="#/account";
+					//DEBUG INFO
+					debug.response = $scope.response;
+
+					if (angular.isDefined(data.customer) && data.loginInfo == "authenticated") {
+						customer.account = data.customer.accounts[0];
+						$state.go('account');
+						//window.location.href = "#/account";
 					}
-                }).
+				}).
 				error(function(data, status, headers, config) {
 					// called asynchronously if an error occurs
 					// or server returns response with an error status.
 					$scope.response = JSON.stringify(data);
-//DEBUG INFO
-                    debug.response = $scope.response;
+					//DEBUG INFO
+					debug.response = $scope.response;
 
-             });
-              
+				});
+
 			};
 		});
-		app.controller('accountController', function($scope, $ionicModal, customer, debug) {
-         $scope.account = customer.account;
-		 $scope.account.currency = customer.currency;
-		 $scope.invoices = customer.account.invoices;
-		 $scope.request = debug.request;
-		 $scope.debug = debug.debug;
-		 $scope.response = debug.response;
-		 
-		 
-		 $ionicModal.fromTemplateUrl('partials/invoiceDetail.html', {
-		 scope: $scope,
-		 animation: 'slide-in-right'})
-		 .then ( function(modal) { 
-		  $scope.modal = modal
-		 });
-		 
-		 $scope.openModal = function (index) {
-		  $scope.invoice = $scope.invoices[index];
-		  $scope.modal.show();
-		 };
-
-		 $scope.closeModal = function () {
-		  $scope.modal.hide();
-		 };
-
-		 $scope.$on ( '$destroy', function () {
-		  $scope.modal.remove();
-		 });
-
-		 
+		app.controller('invoiceController', function($scope, customer, invoice ) {
+			$scope.account = customer.account;
+			$scope.account.currency = customer.currency;
+			$scope.invoice = invoice;
 		});
-	
+
+		app.controller('accountController', function($scope, $ionicModal, customer, debug) {
+			$scope.account = customer.account;
+			$scope.account.currency = customer.currency;
+			$scope.invoices = customer.account.invoices;
+			$scope.request = debug.request;
+			$scope.debug = debug.debug;
+			$scope.response = debug.response;
+
+/*
+			$ionicModal.fromTemplateUrl('partials/invoiceDetail.html', {
+				scope: $scope,
+				animation: 'slide-in-right'
+			})
+				.then(function(modal) {
+					$scope.modal = modal;
+				});
+
+			$scope.openModal = function(index) {
+				$scope.invoice = $scope.invoices[index];
+				$scope.modal.show();
+			};
+
+			$scope.closeModal = function() {
+				$scope.modal.hide();
+			};
+
+			$scope.$on('$destroy', function() {
+				$scope.modal.remove();
+			});
+*/
+
+		});
